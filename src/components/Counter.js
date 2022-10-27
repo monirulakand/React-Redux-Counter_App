ca5407c2a11fb07d60cbff63cdc06499bcf5db29
Monirul Islam Akand <45770524+monirulakand@users.noreply.github.com>
2022-10-27 07:23:05 +0600

    counter project react redux apply done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, setCustom} from "../redux/state/counter/counterSlice";
 
 const Counter = () => {
+
+    const myNumber=useRef();
+    const count=useSelector((state)=> state.counter.value);
+    const dispatch=useDispatch();
+
     return (
         <div className="card">
             <div className="card-header bg-primary">
                 <h4 className="text-white">Counter App</h4>
             </div>
             <div className="card-body">
-                <h1>00</h1>
+                <h1>{count}</h1>
                 <div className="my-4">
-                    <button className="btn btn-success">Increase</button>
-                    <button className="btn mx-2 btn-danger">Decrease</button>
+                    <button onClick={()=>{dispatch(increment())}} className="btn btn-success">Increase</button>
+                    <button onClick={()=>{dispatch(decrement())}} className="btn mx-2 btn-danger">Decrease</button>
+                </div>
+                <div className="my-4">
+                    <input ref={myNumber} className="form-control w-100" type="number"/>
+                    <button onClick={()=>dispatch(setCustom(myNumber.current.value))} className="btn btn-dark my-2 btn-block w-100">Custom Value</button>
                 </div>
             </div>
         </div>
